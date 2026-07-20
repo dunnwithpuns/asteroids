c0262fe1433a86504d4f0912e467cbe904986ab3
dunnwithpuns <dunnwithpuns@gmail.com>
2026-07-20 11:24:31 -0500

    added fps control

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
+    clock = pygame.time.Clock()
+    dt = 0.0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -14,6 +17,8 @@ def main():
 
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
