4d5e2928a4b65656be5d85b881a866948332c880
dunnwithpuns <dunnwithpuns@gmail.com>
2026-07-19 22:14:34 -0500

    actually did the window right this time

--- main.py
+++ main.py
@@ -9,9 +9,6 @@ def main():
     while True:
         log_state()
         for event in pygame.event.get():
-            pass
-        
-        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
